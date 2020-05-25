7a2f9aa7acc87c40ca04f770427fd94e3961874f
muradaghazade <60175525+muradaghazade@users.noreply.github.com>
2020-05-25 19:19:27 +0400

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -5,15 +5,13 @@ let client = new LyricsClient(url);
 document.querySelector(".button").addEventListener("click", function () {
     let keyword = document.querySelector(".artist-input").value;
     let second = document.querySelector(".title-input").value;
-    document.querySelector(".lyrics-input").style.display = "none";
-    document.querySelector(".lyrics-button").style.display = "none";
     client.search(keyword, second);
 });
 
 document.querySelector(".dark-mode-button").addEventListener('click', function(e){
     document.querySelector("body").style.backgroundColor = "black";
         document.querySelector("body").style.color = "white";
-            document.querySelector(".nav-bar").style.backgroundColor = "gray";
+            document.querySelector(".nav-bar").style.backgroundColor = "#686868";
                 document.querySelector(".a").style.color = "white";
                      document.querySelector(".t").style.color = "white";
                          document.querySelector(".l").style.color = "white";
@@ -21,6 +19,7 @@ document.querySelector(".dark-mode-button").addEventListener('click', function(e
                                 document.querySelector(".response-elements").style.color = "white";
                                     document.querySelector(".dark-mode-button-2").style.backgroundColor = "white";
                                         document.querySelector(".dark-mode-button-2").style.color = "dimgray";
+                                            document.querySelector("#first-part").style.backgroundColor = "#101010";
 });
 
 document.querySelector(".dark-mode-button-2").addEventListener('click', function(e){
@@ -36,6 +35,47 @@ document.querySelector(".dark-mode-button-2").addEventListener('click', function
 })
 
 document.querySelector(".lyrics-button").addEventListener('click', function(){
-    let lkeyword = document.querySelector(".lyrics-input").value;
-    client.search_lyrics(lkeyword);
+    let keyword = document.querySelector(".lyrics-input").value;
+    let found = search_keyword(keyword);
+
+    if(found.length > 0) {
+      document.querySelector("#second-part").innerHTML = `
+      <div class="about-track">
+      <h1>${found[0].artist.toUpperCase()}</h1>
+      <h1>${found[0].title.toUpperCase()}</h1>
+      <p>${found[0].lyrics.split("\n").join("<br/>")}</p>
+      </div>
+        <style>
+             h1 {
+                color: #00FF00;
+                }
+        </style>
+      `
+    } else {
+      alert("Not found");
+    }
+
 });
+
+
+search_keyword = (keyword) => {
+
+  let results = [];
+  for(let i=0; i<localStorage.length; i++) {
+     let keyname = localStorage.key(i)  // lyrics_Reminder
+     if(keyname.startsWith("lyrics_")) {
+        let record = localStorage.getItem(keyname); 
+        let entry = JSON.parse(record); //Object: {title: Reminder, artist: The Weeknd, lyrics: "Song lyrics goes here"}
+        if(entry.lyrics.toLowerCase().indexOf(keyword) > -1) { // "song lyrics goes here".indexOf("lyrics")
+          results.push(entry);
+        } 
+     }
+  }
+  return results;
+}
+
+
+
+
+
+
--- js/client.js
+++ js/client.js
@@ -16,7 +16,7 @@ class LyricsClient{
     render_data_to_dom = (element) => {
         let art = document.querySelector(".artist-input").value.toUpperCase();
         let titl = document.querySelector(".title-input").value.toUpperCase();
-        document.querySelector(".elements").innerHTML =  `
+        document.querySelector("#second-part").innerHTML =  `
         <div class="h1-class">
             <h1 class="background">${art}</h1>
             <h1 class="background-two">${titl}</h1>
@@ -25,41 +25,32 @@ class LyricsClient{
                     <p class="song-lyrics">${element.lyrics.split("\n").join("<br/>")}</p>
                 </div>
                 <style>
-                    .response-elements {
-                        margin-left: 70px;
-                        margin-top: 20px;
-                    }
-                    .song-lyrics{
-                        margin-top: -150px;
-                    }
-                    .h1-class {
-                        margin-left: -600px;
-                    }
-                    h1 {
-                        color: #ADFF2F;
-                        font-size: 50px;
-                    }
+                h1 {
+                    color: #00FF00;
+                }
                 </style>`;
     }
 
-    search(keyword, second) {
-        let url = `${this.url}/${keyword}/${second}`;
+    search(artist, title) {
+        let url = `${this.url}/${artist}/${title}`;
         
         fetch(url)
             .then(resp => resp.json())
             .then(data => {
                 this.render_data_to_dom(data);
-                
 
-                            }).catch(err => console.log(err.message));
+                let entry = {
+                    artist: artist,
+                    title: title,
+                    lyrics: data.lyrics
+                }
+                this.save_to_storage(title, entry);
+            }).catch(err => console.log(err.message));
     }
-    search_lyrics(lKeyword) {
-        if (lKeyword === "idk") {
-            //for (let i =375; i < 390; i++) {
-                console.log(localStorage.getItem('lyrics'));
-            
-            //}
-        }
-            
+
+    save_to_storage(key, data) {
+        let k = `lyrics_${key}`;
+        window.localStorage.setItem(k, JSON.stringify(data));
     }
+
 }
